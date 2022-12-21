92f392cbba9f4d8e4d1c8877a8c2111e19fbe9b7
Mohammad Khoulani <mhmmdkholani@gmail.com>
2022-12-21 14:08:52 +0200

    Add the project

---
+++ Src/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	private List<string> _allImages;
+	public List<string> ImageList1 { get; set; }
+	public List<string> ImageList2 { get; set; }
+	public List<string> ImageList3 { get; set; }
+	public List<string> ImageList4 { get; set; }
+
+    public MainPage()
+	{
+		InitializeComponent();
+		GenerateData();
+		ImageList1 = _allImages;
+		ImageList2 = _allImages;
+		ImageList3 = _allImages;
+		ImageList4 = _allImages;
+        BindingContext = this;
+	}
+
+	private void GenerateData()
+	{
+        _allImages = new();
+		for (int i = 1; i <= 18; i++)
+		{
+			_allImages.Add($"img{i.ToString("00")}.png");
+		}
+	}
+	
+}
+
---
+++ Src/MauiApp2/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp2;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				//fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				//fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("AtypDisplay-Bold.ttf", "AtypDisplayBold");
+				fonts.AddFont("AtypDisplay-Regular.ttf", "AtypDisplayRegular");
+            });
+
+		builder.Services.AddSingleton<MainPage>();
+
+		return builder.Build();
+	}
+}
